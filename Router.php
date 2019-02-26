617103934cd43a584d7a68e0b690082d862948d6
AlissaCosson <alissa.cosson@gmail.com>
2019-02-26 18:21:10 +0100

    Résolution des conflits

--- Router.php
+++ Router.php
@@ -4,34 +4,28 @@ require_once ("controller/Controller.php");
 require_once ("view/View.php");
 require_once ("model/Connexion.php");
 require_once ("model/fonc_bdd.php");
-<<<<<<< HEAD
 require_once ("model/WeirdObjectStorageStub.php");
-=======
 require_once ("model/Inscription.php");
->>>>>>> 91b3bd87f9e45624c7c1082df45dcd2065d40d61
 
 class Router
 {
 
     public function main() {
 
-<<<<<<< HEAD
         $view = new View($this);
         $connexion = new Connexion();
         $weirdObjectStorage = new WeirdObjectStorageStub();
 
         $ctrl = new Controller($view, $connexion, $weirdObjectStorage);
         if (key_exists('list', $_GET)) {
-            //$ctrl->connexion();
+            $ctrl->connexion();
             $ctrl->showList();
-=======
+        }
         session_start();
 
         if (key_exists("id",$_SESSION)) {
             echo "Bonjour ".$_SESSION['id'];
         }
-
-
         $view = new View($this);
         $connexion = new Connexion();
         $inscription = new Inscription();
@@ -41,7 +35,6 @@ class Router
         $ctrl->accueil();
         if (key_exists('connexion', $_GET)) {
             $ctrl->connexion();
->>>>>>> 91b3bd87f9e45624c7c1082df45dcd2065d40d61
         }
         if (key_exists('inscription', $_GET)) {
             $ctrl->inscription();
--- controller/Controller.php
+++ controller/Controller.php
@@ -2,38 +2,30 @@
 
 require_once("view/View.php");
 require_once ("model/Connexion.php");
-<<<<<<< HEAD
-
-=======
 require_once ("model/Inscription.php");
->>>>>>> 91b3bd87f9e45624c7c1082df45dcd2065d40d61
+
 class Controller
 {
     protected $view;
     protected $connexion;
-<<<<<<< HEAD
     protected $weirdObjectStorage;
-=======
     protected $inscription;
->>>>>>> 91b3bd87f9e45624c7c1082df45dcd2065d40d61
 
     /**
      * Controller constructor.
      * @param $view
      */
-<<<<<<< HEAD
     public function __construct(View $view, Connexion $connexion, WeirdObjectStorage $weirdObjectStorage)
     {
         $this->view = $view;
         $this->connexion = $connexion;
         $this->weirdObjectStorage = $weirdObjectStorage;
-=======
+
     public function __construct(View $view, Connexion $connexion, Inscription $inscription)
     {
         $this->view = $view;
         $this->connexion = $connexion;
         $this->inscription = $inscription;
->>>>>>> 91b3bd87f9e45624c7c1082df45dcd2065d40d61
     }
 
     public function accueil() {
