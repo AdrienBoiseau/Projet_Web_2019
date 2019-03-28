83d7bbc6aa04b2c12c4ab363c55dd9a9fe8e7b2b
AdrienBoiseau <adrien-97@hotmail.fr>
2019-03-28 11:48:14 +0100

    Avancement générale dans le design

--- view/templateList.php
+++ view/templateList.php
@@ -4,14 +4,17 @@
 	<title>Liste d'objets insolites</title>
 	<meta charset="UTF-8" />
 </head>
-	<h1>
-		<?php echo '<a href=?id=' . $weirdObject->getId(). '>' . $weirdObject->getName() . ', ' . $weirdObject->getDescription(); 
-				echo '</a>';
+<div class="view_object">
+	<h1 class="title_object">
+		<?php echo '<a href=?id=' . $weirdObject->getId(). '>' . $weirdObject->getName() . ', ' . $weirdObject->getDescription();
+
 		?>
 	</h1>
-	<h2><?php echo $weirdObject->getPrice() ?>€</h2>
+	<h2 class="price_object"><?php echo $weirdObject->getPrice() ?>€</h2>
 	<img src=<?php echo $weirdObject->getImage() ?> />
     <?php
+    echo '</a>';
     echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";
     ?>
+</div>
 </html>
